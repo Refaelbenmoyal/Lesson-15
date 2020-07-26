ec96a4f4fb9bfbc41179b0370a825e68e8834bf6
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2020-07-26 15:22:21 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Game
+    {
+        //Data
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+        //Functonallty
+        public Game()
+        {
+
+        }
+        public Game(string name, float rating = 5.5f)
+        {
+            _name = name;
+            _rating = rating;
+        }
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return "$ Game: name:{_name} numberOfPlayers:{_numberOfPlayers} rating:{_rating} originCountry:{originCountry}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void PrintGame(Game Sudoko)
+        {
+            Console.WriteLine(Sudoko);
+        }
+        static void Main(string[] args)
+        {
+            Game Sudoko = new Game(
+                "Sudoko", 1, 5.5f, "Japan");
+            Game Football = new Game()
+            {
+                _name = "Football",
+                _numberOfPlayers = 22,
+                _originCountry = "Englnd",
+                _rating = 5.5f,
+            };
+            Game Cards = new Game("Cards", 5.5f)
+            {
+                _originCountry = "China",
+                _numberOfPlayers = 2,
+            };
+            Game chess = new Game("Chess", "China")
+            {
+                _numberOfPlayers = 2,
+                _rating = 5.5f,
+            };
+
+        }
+
+    }
+}
